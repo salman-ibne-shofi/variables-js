e0c5651febb6813f7101d2c82d20f22fface6e47
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-01-27 17:14:34 +0600

    introduction to If Else condition

---
+++ if.js
@@ -0,0 +1,53 @@
+/***
+ * Tui jodi amake khawas 
+ *      taile ami ashbo
+ * 
+ * ------------------------
+ * if you buy food for me
+ *      i will come
+ * 
+ * --------------------------
+ * if briyani price less than 300 taka
+ *      i will buy
+ * 
+ * 
+ * ---------STRUCTURE------------
+ *      if (condition){
+ * 
+ * }
+ * 
+ * ------------------------------
+ *      if (condition){
+ * 
+ * }
+ * 
+ */
+
+
+// if(13 < 10){
+//     console.log('i have a smaller value');
+// }
+
+// var biriyaniPrice = 200;
+
+// if(biriyaniPrice < 300){
+// //     console.log('mama give me some biriyani')
+// }
+
+/**
+ * if(condition) {
+ *      execute code if conDition above is true
+ * }
+ * else {
+ *      execute code if the condition ifs false
+ * }
+ */ 
+
+var weight = 40;
+
+if(weight < 20){
+    console.log('i will carry it by mylself')
+}
+else{
+    console.log('i will rent a rickshaw')
+}
