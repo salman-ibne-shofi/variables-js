be4e192af75ad87dd94ac757ce95415e5d713437
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-01-27 21:28:06 +0600

    logical not operator

---
+++ shortcut.js
@@ -0,0 +1,15 @@
+const isLeader = true;
+// if(isLeader){
+//     console.log('leader ashse! chair charo')
+// }
+// else{
+//     console.log('vitore dhute dibi na')
+// }
+
+const isPassed = true;
+if(!isPassed){
+    console.log('toke biye dia dimu');
+}
+else {
+    console.log('tumi amader noyon er moni');
+}
