094c03b38ec90ba79cb4117ad64a7213b27be2d6
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-01-27 15:06:32 +0600

    variables-js

---
+++ variable.js
@@ -0,0 +1,35 @@
+/**
+ * variable ==> container
+ */
+
+// number
+var sunglassPrice = 80;
+
+// string
+var subject = "EEE";
+
+// boolean
+var passed = true;
+
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with a number
+ * 5. can not a keyword
+ * 6. prefer camelCase
+ */
+
+
+var lastExamResul = 89;
+
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN ?
+ * 4. null
+ * 5. Number
+ */
